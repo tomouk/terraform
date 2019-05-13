2a9b3e442cfd067c899cd2af8f992bd773b1d8a2
tomouk <james372@hotmail.com>
2019-05-13 21:25:03 +0100

    Update instance.tf

--- createEC2/instance.tf
+++ createEC2/instance.tf
@@ -1,10 +1,4 @@
-provider "aws" {
-  access_key = ""
-  secret_key = ""
-  region     = "us-east-1a"
-}
-
 resource "aws_instance" "example" {
-  ami           = "ami-014b38e758721be30"
+  ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
   instance_type = "t2.micro"
 }
